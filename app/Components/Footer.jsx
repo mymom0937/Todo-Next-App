84976043011340cf8e47db9c58af5a09dffc4ce3
mymom0937 <mymom0937@gmail.com>
2025-03-25 19:31:33 +0000

    updated

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-200 dark:bg-gray-800 text-gray-600 dark:text-gray-400 p-3 sm:p-4">
       <div className="container mx-auto text-center">
-        <p className="text-xs sm:text-sm">© {new Date().getFullYear()}, Seid E.</p>
+        <p className="text-sm md:text-md font-semibold">© {new Date().getFullYear()}, Seid E.</p>
       </div>
     </footer>
   );
